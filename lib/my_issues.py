79480a05b3e5eec7b5ec5ff3f9a95d20ac909008
Pranav Raja <rickdangerous1@gmail.com>
2012-05-31 17:53:35 +1000

    better ordering for my issues

--- lib/my_issues.py
+++ lib/my_issues.py
@@ -2,7 +2,7 @@ import jira
 import sys
 import json
 
-def main(query='not (status = Closed) and assignee = currentUser()'):
+def main(query='not (status = Closed) and assignee = currentUser() order by updated desc'):
 	api = jira.JiraAPI.default_api()
 	searcher = jira.IssueSearcher(api)
 	resp = searcher.search(query)
